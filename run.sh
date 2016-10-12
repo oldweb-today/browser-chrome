e5338bd043b0ad1826ebe95a3580b11803c41ee2
Ilya Kreymer <ikreymer@gmail.com>
2016-10-12 10:04:23 -0700

    use new webrecorder base image add metadata via labels, use system $http_proxy/$https_proxy for proxy settings use $PROXY_GET_CA to determine if ca cert needs to be loaded tags: default tags file for docker image

--- run.sh
+++ run.sh
@@ -3,20 +3,18 @@
 #fluxbox -display $DISPLAY -log /tmp/fluxbox.log &
 jwm -display $DISPLAY &
 
-#http_proxy="http://netcapsule_pywb_1:8080" wget "http://pywb.proxy/pywb-ca.pem"
-curl -x "proxy:8080"  "http://mitm.it/cert/pem" > /tmp/proxy-ca.pem
+if [[ -n "$PROXY_GET_CA" ]]; then
+    curl -x "$PROXY_HOST:$PROXY_PORT"  "$PROXY_GET_CA" > /tmp/proxy-ca.pem
 
-mkdir -p $HOME/.pki/nssdb
-certutil -d $HOME/.pki/nssdb -N
-certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n "PROXY" -i /tmp/proxy-ca.pem 
+    mkdir -p $HOME/.pki/nssdb
+    certutil -d $HOME/.pki/nssdb -N
+    certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n "Proxy" -i /tmp/proxy-ca.pem
+fi
 
 mkdir ~/.config/
 mkdir ~/.config/google-chrome
 touch ~/.config/google-chrome/First\ Run
 
-if [ -n "$NO_PROXY" ]; then
-    run_browser google-chrome "$URL"
-else
-    run_browser google-chrome --proxy-server="proxy:8080" --disable-popup-blocking --disable-background-networking --disable-client-side-phishing-detection --disable-component-update --safebrowsing-disable-auto-update "$URL"
-fi
+run_browser google-chrome --disable-popup-blocking --disable-background-networking --disable-client-side-phishing-detection --disable-component-update --safebrowsing-disable-auto-update "$URL"
+
 
