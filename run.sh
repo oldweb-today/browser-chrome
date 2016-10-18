a509b6f1b4a0834da441988aed6952ecbf1cd68b
Ilya Kreymer <ikreymer@gmail.com>
2016-10-18 21:30:21 +0000

    add fixed chrome 53 deb and install chrome from the deb instead of latest (due to issues with flash in chrome 54) now using ubuntu base jwmrc moved to base image, run jwmrc in restart loop

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #fluxbox -display $DISPLAY -log /tmp/fluxbox.log &
-jwm -display $DISPLAY &
+run_browser jwm -display $DISPLAY &
 
 if [[ -n "$PROXY_GET_CA" ]]; then
     curl -x "$PROXY_HOST:$PROXY_PORT"  "$PROXY_GET_CA" > /tmp/proxy-ca.pem
@@ -15,6 +15,6 @@ mkdir ~/.config/
 mkdir ~/.config/google-chrome
 touch ~/.config/google-chrome/First\ Run
 
-run_browser google-chrome --disable-popup-blocking --disable-background-networking --disable-client-side-phishing-detection --disable-component-update --safebrowsing-disable-auto-update "$URL"
+run_browser google-chrome --no-default-browser-check --disable-popup-blocking --disable-background-networking --disable-client-side-phishing-detection --disable-component-update --safebrowsing-disable-auto-update "$URL"
 
 
