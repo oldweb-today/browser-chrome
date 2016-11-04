a6a26ee6479550956c304ffdefbb40645374148e
Ilya Kreymer <ikreymer@gmail.com>
2016-11-04 09:58:23 -0700

    audio: move down to base-browser proxy ca: check for PROXY_HOST before initing proxy ca

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#fluxbox -display $DISPLAY -log /tmp/fluxbox.log &
 run_browser jwm -display $DISPLAY &
 
-if [[ -n "$PROXY_GET_CA" ]]; then
+if [[ -n "$PROXY_GET_CA" && -n "$PROXY_HOST" ]]; then
     curl -x "$PROXY_HOST:$PROXY_PORT"  "$PROXY_GET_CA" > /tmp/proxy-ca.pem
 
     mkdir -p $HOME/.pki/nssdb
@@ -18,9 +17,6 @@ touch ~/.config/google-chrome/First\ Run
 run_browser google-chrome --no-default-browser-check --disable-popup-blocking --disable-background-networking --disable-client-side-phishing-detection --disable-component-update --safebrowsing-disable-auto-update "$URL" &
 
 pid=$!
-
-#run_browser /app/ffmpeg -f pulse -i default -bufsize 32k -ac 1 -c:a libopus -min_frag_duration 500 -ab 12k -ar 12000 -listen 1 -f webm http://0.0.0.0:6082 &
-run_browser /app/ffmpeg -re -f pulse -i default -bufsize 32k -ac 1 -c:a libopus -ab 32k -listen 1 -f segment -min_frag_duration 500 -f webm http://0.0.0.0:6082 &
 
 count=0
 wid=""
