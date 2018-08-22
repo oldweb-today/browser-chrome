c831a05a40ee7f908f3e01b5712c5267c0870504
John Berlin <n0tan3rd@gmail.com>
2018-08-22 00:30:25 -0400

    updated run.sh to use automation-ready cmdline arguments
    added chrome 67 deb (#1)

--- run.sh
+++ run.sh
@@ -17,7 +17,30 @@ touch ~/.config/google-chrome/First\ Run
 # tunnel to localhost
 run_browser socat tcp-listen:9222,fork tcp:localhost:9221 &
 
-run_browser google-chrome --no-default-browser-check --disable-popup-blocking --disable-background-networking --disable-client-side-phishing-detection --disable-component-update --safebrowsing-disable-auto-update --remote-debugging-port=9221 "$URL" &
+run_browser google-chrome --no-default-browser-check \
+  --disable-component-update \
+  --disable-popup-blocking \
+  --disable-background-networking \
+  --disable-background-timer-throttling \
+  --disable-client-side-phishing-detection \
+  --disable-default-apps \
+  --disable-extensions \
+  --disable-hang-monitor \
+  --disable-prompt-on-repost \
+  --disable-sync \
+  --disable-translate \
+  --disable-domain-reliability \
+  --disable-renderer-backgrounding \
+  --disable-infobars \
+  --disable-translate \
+  --metrics-recording-only \
+  --no-first-run \
+  --safebrowsing-disable-auto-update \
+  --password-store=basic \
+  --use-mock-keychain \
+  --autoplay-policy=no-user-gesture-required  \
+  --disable-features=site-per-process \
+  --remote-debugging-port=9221 "$URL" &
 
 pid=$!
 
